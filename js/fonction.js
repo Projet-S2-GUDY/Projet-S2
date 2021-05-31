43e1115bdddb1472b15bcb7d35b793ff58bd42a3
dany <dany.guillet@univ-lyon2.fr>
2021-05-31 10:59:12 +0200

    debbug

--- js/fonction.js
+++ js/fonction.js
@@ -1,7 +1,6 @@
 function newgameprompt(linkto){
   var per = prompt("Please enter a username\n(maximum 20 alphanumeric or underscore characters)")
   if (per !==null && per !== ""){
-    window.location.href=linkto
+    window.location.href="newgame.php"
   }
-
 }
--- php/Menu.php
+++ php/Menu.php
@@ -3,15 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>Guess the Correlation</title>
-    <script src="../js/fonction.js">
-
-    </script>
+    <script src="../js/fonction.js"></script>
   </head>
   <body>
     <img src="" alt="Image Guess the correlation">
     <table>
       <tr>
-        <td onclick="newgameprompt(newgame.php)">New Game</td>
+        <td onclick="newgameprompt()">New Game</td>
       </tr>
       <tr>
         <td>Two Players</td>
