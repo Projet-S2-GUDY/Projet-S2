3f0e2b59aac1597f8f5c862c9212c6f0299792d3
dany <dany.guillet@univ-lyon2.fr>
2021-05-31 09:58:56 +0200

    Création des pages vierge

---
+++ php/Menu.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Guess the Correlation</title>
+  </head>
+  <body>
+    <img src="" alt="Image Guess the correlation">
+    <table>
+      <tr>
+        <td><a href="newgame.html">New Game</a></td>
+      </tr>
+      <tr>
+        <td>Two Players</td>
+      </tr>
+      <tr>
+        <td>Score Board</td>
+      </tr>
+      <tr>
+        <td>About</td>
+      </tr>
+      <tr>
+        <td>Settings</td>
+      </tr>
+    </table>
+  </body>
+</html>
