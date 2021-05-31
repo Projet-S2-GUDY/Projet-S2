d050c059fab661a50676bedeab88137890a4c706
UgoLabbe <ugo.labbe@univ-lyon2.fr>
2021-05-31 11:24:28 +0200

    partie about

--- php/about.php
+++ php/about.php
@@ -5,6 +5,34 @@
     <title>Guess the correlation</title>
   </head>
   <body>
+<h1>About the game<h1>
+    <br>
+  <p>the aim of the game is simple. try to guess how correlated the two variables in a scatter plot are. the closer your guess is to the true correlation, the better.<p>
+    <br>
+  <p>your guess should be between zero and one, where zero is no correlation and one is perfect correlation. no negative correlations are used in the game. here are some examples:<p>
+<!--mettre les images -->
+    <br>
+  <p>guess the correlation is a game with a purpose. this means, while it aims to be entertaining, data on the guesses is collected and used to analyse how we perceive correlations in scatter plots. so the more people that play, the more data is generated!<p>
+    <br>
+  <h1>Rules<h1>
+    <br>
+  <p>guess within 0.05 of the true correlation: +1 life and +5 coins<p>
+  <p>guess within 0.10 of the true correlation: +1 coin<p>
+  <p>guess within >0.10 of the true correlation: -1 life<p>
+    <br>
+  <p>you will also receive bonus coins if you make good guesses in a row!<p>
+    <br>
+  <h1>About me<h1>
+    <br>
+  <p>i'm a phd student studying bioinformatics at the university of cambridge and the european bioinformatics institute. this game is a side project to feed one of my many day-to-day curiosities.
+i'm always grateful for suggestions and happy to answer questions about the game or how the data will be used. so tweet me at @omarwagih or email me.<p>
+<!--Changer ce texte-->
+    <br>
+  <h1>Usage disclaimer and privacy statement<h1>
+    <br>
+  <p>the analysis of the resulting data relies on true human guesses. please don't go out of your way to repeatedly and systematically guess the correct correlation (e.g. using bots). it takes away from the enjoyment of other users by spamming the score board. it's also easy to identify users that are doing this from the data and clear their name from the score board. that being said, if you find a bug or something does not work as expected, please do email me so i can sort it out.<p>
+    <br>
+  <p>the following information will be automatically stored remotely whenever you make a correlation guess: current date and time, ip address and correlation data. this data is evaluated purely for statistical purposes in an anonymised form. if you do not consent to providing such information, or contributing data please refrain from playing. for more information on how the data will be used, feel free to email me<p>
 
   </body>
 </html>
