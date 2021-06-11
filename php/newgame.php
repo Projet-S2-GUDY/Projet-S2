22c0c22a71ad562e0b2fcfda37059e0b34ded55b
dany <dany.guillet@univ-lyon2.fr>
2021-06-11 09:41:48 +0200

    random number plot

--- php/newgame.php
+++ php/newgame.php
@@ -4,16 +4,22 @@
     <meta charset="utf-8">
     <title>Guess the correlation</title>
       <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+      <script src="../js/fonction.js">
+
+      </script>
   </head>
   <body>
+    <a href="menu.php">Main Menu</a>
     <div id="myPlot" style="width:100%;max-width:700px"></div>
 
     <script>
-
     // faire une fonction qui va boucler la matrice avec 100 points aleatoire entre 0 et 1
-    var xArray = [0.50,0.60,0.70,0.80,0.90,0.100,0.110,0.120,0.130,0.140,0.150];
-    var yArray = [0.7,0.8,0.8,0.9,0.9,0.9,0.10,0.11,0.14,0.14,0.15];
-
+    var xArray = [];
+    var yArray = [];
+    for (var i=0, t=40; i<t; i++) {
+      xArray.push(Math.random())
+      yArray.push(Math.random())
+    }
     // Define Data
     var data = [{
       x:xArray,
@@ -31,7 +37,7 @@
 
     // Display using Plotly
     Plotly.newPlot("myPlot", data, layout,{displayModeBar: false});
-    document.write(math.random ());
+    document.write(randomNumber());
     </script>
   </body>
 </html>
