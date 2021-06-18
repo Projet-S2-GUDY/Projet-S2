44308bd5adb2b698910a7e8b7f20db1087458b57
dany <dany.guillet@univ-lyon2.fr>
2021-06-18 11:24:43 +0200

    refresh plot and refresh result

--- Jeu/newgame.php
+++ Jeu/newgame.php
@@ -2,24 +2,23 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css" type="text/css" />
     <title>Guess the correlation</title>
       <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
       <script src="fonction.js"></script>
   </head>
   <body>
-    <h1><div align="right"><a href="Menu.php">Main Menu</a></div><h1>
+    <a href="menu.php">Main Menu</a>
       <div id="points">
         <p>VIE : <span id="life">3</span></p>
         <p>COINS : <span id="coins">0</span></p>
       </div>
+
       <div id="myPlot" style="width:100%;max-width:40%"></div>
       <!-- //scatterplot -->
       <script>
         var xArray = [];
         var yArray = [];
-        var xArr = [];
-        var yArr = [];
+
         var rho = Math.round(Math.random()*100)/100;
         var y1 = 1;
         var y2 = rho;
@@ -40,24 +39,71 @@
 
         // Define Layout https://plotly.com/javascript/axes/
         var layout = {
-          xaxis: {range: [0, 1], title: "titre1", dtick: 0.5,fixedrange: true},
-          yaxis: {range: [0, 1], title: "titre2", dtick: 0.5,fixedrange: true},
+          xaxis: {range: [0, 1], dtick: 0.5,fixedrange: true},
+          yaxis: {range: [0, 1], dtick: 0.5,fixedrange: true},
           autosize: true,
-          title: "titre graph",
           hovermode: false,
         };
 
         // Display using Plotly
         Plotly.newPlot("myPlot", data, layout,{displayModeBar: false});
-        document.write("réponse: ",rho);
-      </script>
-      <div class="bouton">
-        <input type="text" id="try" name="try" value="0.">
-        <input type="button" id="reponse" name="reponse" value="GUESS" onclick="test()">
-      </div>
-      <div id="new">
 
-      </div>
+        function adjustValue(){
+          Plotly.deleteTraces("myPlot", 0);
+         }
+
+         function updatenewplot(){
+           var xArray = [];
+           var yArray = [];
+           var newrho = Math.round(Math.random()*100)/100;
+           rho = newrho;
+           var y1 = 1;
+           var y2 = rho;
+           var x1 = 0;
+           var x2 = Math.sqrt(1-rho**2)
+           for (var i=0, t=50; i<t; i++) {
+             xbis = Math.random();
+             ybis = Math.random();
+             xArray.push(xbis*y1+ybis*x1)
+             yArray.push(xbis*y2+ybis*x2)
+           }
+           var data = [{
+             x:xArray,
+             y:yArray,
+             mode:"markers"
+           }];
+
+           // Define Layout https://plotly.com/javascript/axes/
+           var layout = {
+             xaxis: {range: [0, 1], dtick: 0.5,fixedrange: true},
+             yaxis: {range: [0, 1], dtick: 0.5,fixedrange: true},
+             autosize: true,
+             hovermode: false,
+           };
+           Plotly.newPlot("myPlot", data, layout,{displayModeBar: false});
+         }
+
+         function newplotfunc(){
+           adjustValue();
+           updatenewplot();
+           test();
+           document.getElementById('bouton').style.visibility= "visible";
+           document.getElementById('nextbouton').style.visibility= "hidden";
+           document.getElementById('new').style.visibility= "hidden";
+         }
+        </script>
+
+        <div id="nextbouton" style="visibility: hidden; display:inline;">
+          <input type="button" name="next" value="NEXT" onclick="newplotfunc()">
+        </div>
+
+        <div id="bouton">
+          <input type="text" id="try" name="try" value="0.">
+          <input type="button" id="reponse" name="reponse" value="GUESS" onclick="test()">
+        </div>
+        <div id="new" style="visibility: hidden; display:inline;">
+
+        </div>
       <!-- comparaison réponse -->
       <script>
         function test(){
@@ -66,9 +112,18 @@
           var life = eval(document.getElementById('life').innerHTML);
           var coin = eval(document.getElementById('coins').innerHTML);
           var tag = document.createElement("p");
-          var result = Math.abs(rho-rep);
-          var text = document.createTextNode("True R : "+ rho +" <br>Guessed R: "+rep+"Difference: "+result);
+          var result =   Math.round(Math.abs(rho-rep)*100)/100;
+
+          var text = document.createTextNode("True R : "+ rho);
           var element = document.getElementById("new");
+          tag.appendChild(text);
+          var brl = document.createElement("br");
+          tag.appendChild(brl);
+          text = document.createTextNode(" Guessed R: "+rep);
+          tag.appendChild(text);
+          var brl = document.createElement("br");
+          tag.appendChild(brl);
+          var text = document.createTextNode("Difference: "+result);
           tag.appendChild(text);
 
           if (result <=0.05) {
@@ -79,9 +134,13 @@
           }else {
             life = life - 1;
           }
+
           element.appendChild(tag);
           document.getElementById('life').innerHTML = life;
           document.getElementById('coins').innerHTML = coin;
+          document.getElementById('bouton').style.visibility= "hidden";
+          document.getElementById('nextbouton').style.visibility= "visible";
+          document.getElementById('new').style.visibility= "visible";
         }
       </script>
   </body>
