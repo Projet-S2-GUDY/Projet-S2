5c4f98c2556474650890844cb24a1aa16951cdcb
UgoLabbe <ugo.labbe@univ-lyon2.fr>
2021-06-18 08:55:40 +0200

    mise en place des liens

--- php/about.php
+++ php/about.php
@@ -8,23 +8,23 @@
   </head>
   <body>
 <h2>About the game<h1>
-  <p>the aim of the game is simple. try to guess how correlated the two variables in a scatter plot are. the closer your guess is to the true correlation, the better.</p>
+  <p>the aim of the game is simple. try to guess <FONT color="blue"><a href="https://en.wikipedia.org/wiki/Pearson_correlation_coefficient">how correlated the two variables in a scatter plot are.</FONT></a>the closer your guess is to the true correlation, the better.</p>
     <br>
   <p>Your guess should be between zero and one, where zero is no correlation and one is perfect correlation. no negative correlations are used in the game. here are some examples:</p>
 <!--mettre les images -->
     <br>
-  <p>guess the correlation is a game with a purpose. this means, while it aims to be entertaining, data on the guesses is collected and used to analyse how we perceive correlations in scatter plots. so the more people that play, the more data is generated!<p>
+  <p>guess the correlation is a <a href="https://en.wikipedia.org/wiki/Human-based_computation_game">game with a purpose.</a> this means, while it aims to be entertaining, data on the guesses is collected and used to analyse how we perceive correlations in scatter plots. so the more people that play, the more data is generated!<p>
     <br>
   <h2>Rules<h1>
   <p>guess within 0.05 of the true correlation: +1 life and +5 coins<br>guess within 0.10 of the true correlation: +1 coin<br>guess within >0.10 of the true correlation: -1 life<br>You will also receive bonus coins if you make good guesses in a row!<p>
     <br>
   <h2>About me<h1>
   <p>We are a group a Student studying statistics and business intelligence. This game is a second semester project of our studies.
-We're always grateful for suggestions and happy to answer questions about the game or how the data will be used. so email us if you need any information.<p>
+We're always grateful for suggestions and happy to answer questions about the game or how the data will be used. so <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">email us</a> if you need any information.<p>
     <br>
   <h2>Usage disclaimer and privacy statement<h1>
   <p>the analysis of the resulting data relies on true human guesses. please don't go out of your way to repeatedly and systematically guess the correct correlation (e.g. using bots). it takes away from the enjoyment of other users by spamming the score board. it's also easy to identify users that are doing this from the data and clear their name from the score board. that being said, if you find a bug or something does not work as expected, please do email me so i can sort it out.</p>
     <br>
-  <p>The following information will be automatically stored remotely whenever you make a correlation guess: current date and time, ip address and correlation data. this data is evaluated purely for statistical purposes in an anonymised form. if you do not consent to providing such information, or contributing data please refrain from playing. for more information on how the data will be used, feel free to email me</p>
+  <p>The following information will be automatically stored remotely whenever you make a correlation guess: current date and time, ip address and correlation data. this data is evaluated purely for statistical purposes in an anonymised form. if you do not consent to providing such information, or contributing data please refrain from playing. for more information on how the data will be used, feel free to <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">email me</a></p>
   </body>
 </html>
