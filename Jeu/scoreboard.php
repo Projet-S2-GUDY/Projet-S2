6ec243ee57c6d81c5b277d1b43b50683e869265d
Gabinrun <gabin.garrot@univ-lyon2.fr>
2021-06-21 10:55:40 +0200

    Top 15 joueurs

--- Jeu/scoreboard.php
+++ Jeu/scoreboard.php
@@ -1,3 +1,6 @@
+<?php
+include("connexion.php"); //Inclusion du fichier qui contient le code pour la connexion
+?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
@@ -7,5 +10,21 @@
   </head>
   <body>
     <h1><div align="right"><a href="Menu.php">Main Menu</a></div><h1>
+
+      <?php
+
+        $mysqli = connectMaBase(); // Appel de la fonction pour se connecter
+
+      // On récupère tout le contenu de la table partie
+      $reponse = $mysqli->query('SELECT  pseudo, Score from partie order by Score limit 15');
+
+      // Parcourir le tableau des enregistrements et affichage de chaque message
+      while($ligne = $reponse->fetch_array(MYSQLI_BOTH)) {
+          echo $ligne['pseudo'].": ";
+          echo $ligne['Score']."<br>";
+        }
+      $mysqli->close(); // Termine le traitement de la requête
+
+      ?>
   </body>
 </html>
