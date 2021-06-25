3e3da464b5aa3724f348b387d1c20f69619aab69
UgoLabbe <ugo.labbe@univ-lyon2.fr>
2021-06-25 14:47:01 +0200

    Score Board into scoreboard

--- Jeu/Menu.php
+++ Jeu/Menu.php
@@ -22,7 +22,7 @@ include("connexion.php"); //Inclusion du fichier qui contient le code pour la co
     </script>
     <center><img src="https://i.ibb.co/xSnxs0J/LOGO-GTC.png" alt="LOGO-GTC" border="0" ></center>
     <FONT size="4pt"><h1 onclick="newgameprompt()"><a style="cursor: pointer;">New Game</a></h1>
-    <a href="scoreboard.php"><h1>Score Board</h1></a>
+    <a href="scoreboard.php"><h1>ScoreBoard</h1></a>
     <a href="about.php"><h1>About</h1></a>
 
 
